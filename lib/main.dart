f771748ec8716202f7394958db57d341d20f6251
Simone <simone.bervi@gmail.com>
2021-01-19 17:14:45 +0100

    First commit

---
+++ lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_website/Navbar/Navbar.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(primarySwatch: Colors.blue, fontFamily: "Montserrat"),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+              begin: Alignment.centerLeft,
+              end: Alignment.centerRight,
+              colors: [
+                Color.fromRGBO(195, 20, 50, 1.0),
+                Color.fromRGBO(36, 11, 54, 1.0)
+              ]),
+        ),
+        child: Column(
+          children: <Widget>[Navbar()],
+        ),
+      ),
+    );
+  }
+}
